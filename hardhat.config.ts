d8f7d4c6379683458108f45c445e8875e490029d
Ivan Sukhanov <sukhanov1993@gmail.com>
2021-11-17 13:51:46 +0300

    small fixes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,15 +30,15 @@ const config: HardhatUserConfig = {
       chainId: 4,
       url: `https://rinkeby.infura.io/v3/bc4d11088848481a972333f5161120fe`,
       accounts: {
-        mnemonic: `absurd anchor bullet lobster unable exclude weird lucky bar soda dumb first`
+        mnemonic: ``
       },
       gasPrice: 2000000000,
     },
     testnet: {
       chainId: 97,
-      url: `https://data-seed-prebsc-2-s3.binance.org:8545/`,
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
       accounts: {
-        mnemonic: `absurd anchor bullet lobster unable exclude weird lucky bar soda dumb first`
+        mnemonic: ``
       },
       gasPrice: 20000000000,
     },
@@ -49,10 +49,21 @@ const config: HardhatUserConfig = {
         mnemonic: ``
       },
       gasPrice: 5000000000,
-    }
+    },
+    polygonMumbai: {
+      chainId: 80001,
+      url: `https://rpc-mumbai.maticvigil.com/`,
+      accounts: {
+        mnemonic: ``
+      },
+      gasPrice: 2000000000,
+    },
   },
   etherscan: {
-    apiKey: "I1SZKJHEB7EYJPX7NGK68T3PEB2SPZISJM"
+    //apiKey: "I1SZKJHEB7EYJPX7NGK68T3PEB2SPZISJM"
+    apiKey: "PKJV8PM5CA3TY3V8NDZ2D7C7FVXFXWH42A" //polygon
+    //apiKey: "7VBBKFBGRXQ2IY24B43SA1551BZTP3WHI3" //bsc
+    //apiKey: "IWRWD4E4KM373MZZXSYNK3CF25H3I51FC4" //etherscan
   }
 }
 
--- scripts/oxmessage-script.js
+++ scripts/oxmessage-script.js
@@ -19,6 +19,8 @@ async function main() {
 
   await oxmessage.deployed();
 
+  console.log('oxmessage: ' + oxmessage.address);
+
   //console.log("Greeter deployed to:", greeter.address);
 }
 
